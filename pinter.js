acf373e442aa8d6f15619426b4093f9c95bfd707
Ümit Dincel <umitdincel@Umits-MacBook-Pro-2.local>
2018-03-02 02:34:44 +0300

    little changes

--- pinter.js
+++ pinter.js
@@ -1,19 +1,22 @@
 var waitForEl = function(selector, callback) {
-  if ($(selector).length) {
-    callback();
-  } else {
-    setTimeout(function() {
-      waitForEl(selector, callback);
-    }, 100);
-  }
+	if ($(selector).length) {
+		callback();
+	} else {
+		setTimeout(function() {
+			waitForEl(selector, callback);
+		}, 100);
+	}
 };
 
 waitForEl('div[message]', function() {
-  	var messages = $('div[message]:not(.clearfix)');
+	var message_rows = $('div[message].clearfix');
 
-  	$.each(messages, function(index, message){
-  		$(message).on('click', function(){
-  			console.log($(this).find('span').text());
-  		});
-  	});
+	$.each(message_rows, function(index, message_row){
+		$(message_row).on('click', function(){
+			var message_box = $(this).find('div[body]')[0];
+			var message_text = $(message_box).find('span').text();
+
+			console.log(message_text, $(message_box).position());
+		});
+	});
 });
